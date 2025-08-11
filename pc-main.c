b9625dff8603845de64e6ee946a753692d460817
heathershaw821 <158240062+heathershaw821@users.noreply.github.com>
2025-08-11 17:15:32 -0400

    Create pc-main.c

---
+++ pc-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argv, char** args) {
+  // println from arduino is printf in C
+  //     ** we write new lines ourselves in C with \n
+  printf("Hello World!\n");
+  return 0;
+}
+
+
