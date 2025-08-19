a03d8fb3f9331ff143bc5fe8a208a4ea5737529f
heathershaw821 <158240062+heathershaw821@users.noreply.github.com>
2025-08-19 18:27:01 -0400

    Update arduino-hello.c

--- arduino-hello.c
+++ arduino-hello.c
@@ -1,21 +1,11 @@
-
+// code that is run once when the device is turned on
 void setup() {
   // Initialize serial communication at 9600 baud rate
   Serial.begin(9600);
 }
 
-
-bool is_even(int n) {
-  if (n % 2 == 0)
-    return true;
-  else
-    return false;
-}
-
+// this code is run repeatedly in a "loop"
 void loop() {
-  if ( is_even(8) ) {
-    Serial.println("yay its even!");
-  }
   Serial.println("Hello World!");
   delay(1000);
 }
