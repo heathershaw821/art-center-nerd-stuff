0e5bfc80d34e67dd9691c260b82aa99c41d14172
heathershaw821 <158240062+heathershaw821@users.noreply.github.com>
2026-03-18 17:21:00 -0400

    Update cereal.py

--- 01 Building a Real Robot/cereal.py
+++ 01 Building a Real Robot/cereal.py
@@ -11,7 +11,9 @@ with serial.Serial() as ser:
   ser.port = '/dev/serial0'
   ser.open()
   sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
-  
+  # MUST be byte strings b'command arg1 arg2 ...'
+  # NOT default Unicode strings, they are more than a byte in length
+  #  SEE -> https://en.wikipedia.org/wiki/UTF-8#Implementations_and_adoption:~:text=Code%20point%20%E2%86%94-,UTF%2D8%20conversion,-First%20code%20point
   ser.write(b'180')
   sleep(3)
   ser.write(b'90')
