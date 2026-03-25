63a4e309d35e4fec906850ddab5a5d3f2198dc23
heathershaw821 <158240062+heathershaw821@users.noreply.github.com>
2026-03-25 17:26:26 -0400

    Create protocol.py

---
+++ 01 Building a Real Robot/lesson 2/protocol.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import serial
+import io
+from time import sleep
+
+class Protocol:
+	def __init__(self):
+		actions =  {  # our "commands" (remember these are bidirectional on agreed terms)
+		# OUTPUTS (Actions)
+			"head": None,     # <<< right now
+			"forward": None,
+			"backward": None,
+			"left": None,
+			"right": None,
+			"blink": None,
+			# INPUTS (Sensors)
+			"distance": None, # <<< right now
+			# .........
+		}
+
+		SOL = ""   # start of action "string" (not always a literal "string")
+		EOL = "\n" # End of action "string"
+
+class Serial(Protocol):
+	def __init__(self):
+		# for every protocol application, you will change these to fit
+		self.SOL = ""
+		self.EOL = "\n"
+		
+if __name__ == "__main__":
+	print("This is a library.... Do better.")
+
+
