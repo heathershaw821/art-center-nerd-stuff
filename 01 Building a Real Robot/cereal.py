6004bde4f915983a61949edbbc8f52fad6e57801
heathershaw821 <158240062+heathershaw821@users.noreply.github.com>
2026-04-09 16:34:53 -0400

    Refactor serial communication for reading data
    Replaced the use of TextIOWrapper with direct serial read.

--- 01 Building a Real Robot/cereal.py
+++ 01 Building a Real Robot/cereal.py
@@ -10,15 +10,14 @@ with serial.Serial() as ser:
   ser.baudrate = 115200
   ser.port = '/dev/serial0'
   ser.open()
-  sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+
   # MUST be byte strings b'command arg1 arg2 ...'
   # NOT default Unicode strings, they are more than a byte in length
   #  SEE -> https://en.wikipedia.org/wiki/UTF-8#Implementations_and_adoption:~:text=Code%20point%20%E2%86%94-,UTF%2D8%20conversion,-First%20code%20point
   ser.write(b'180')
   sleep(3)
   ser.write(b'90')
-  sio.flush() # it is buffering. required to get the data out *now*
-  hello = sio.readline()
+  hello = ser.readline()
   print(hello)
 
 
